7d103ef27601e4128afa030146be27390af9e95c
nicolaxortiz <nicolaxortiz@gmail.com>
2022-10-13 17:07:53 -0500

    Segunda version

--- js/juego.js
+++ js/juego.js
@@ -1,26 +1,50 @@
+const divMensaje = document.querySelector(".divMensaje")
+
 let iconos = []
         let selecciones = []
 
         generarTablero()
 
+        const mensajes = [
+            "No todas las personas en internet son buenas",
+            "No todas las personas en internet son buenas",
+            "No mires ni descargues archivos o imagenes extraños o de sitios web sin supervision de un adulto",
+            "No mires ni descargues archivos o imagenes extraños o de sitios web sin supervision de un adulto",
+            "La gente se hace pasar por personas que no son para engañarte",
+            "La gente se hace pasar por personas que no son para engañarte",
+            "Siempre explora en internet con la supervicion de un adulto, es para que aprendas y seas ayudado",
+            "Siempre explora en internet con la supervicion de un adulto, es para que aprendas y seas ayudado",
+            "No ingreses en paginas peligrosas o raras, van a intentar engañarte",
+            "No ingreses en paginas peligrosas o raras, van a intentar engañarte",
+            "No compartas imagenes tuyas o de tu familia",
+            "No compartas imagenes tuyas o de tu familia",
+            "La informacion de tarjeta de tus padres es privada, nadie mas la necesita",
+            "La informacion de tarjeta de tus padres es privada, nadie mas la necesita",
+            "No aceptes videollamadas de gente que no conoces",
+            "No aceptes videollamadas de gente que no conoces",
+            "Habla con tus padres si estas en peligro, ellos pueden ayudarte, no estas solo",
+            "Habla con tus padres si estas en peligro, ellos pueden ayudarte, no estas solo",
+            "Tu informacion privada es solo para ti, nunca se la envies a nadie mas",
+            "Tu informacion privada es solo para ti, nunca se la envies a nadie mas",
+        ];
+
         function cargarIconos() {
             iconos = [
-                '<img src="https://i.ibb.co/pK31kwZ/00.png" width="100%">',
-                '<img src="https://i.ibb.co/4SG5shk/580b57fbd9996e24bc43c02c.png" width="100%">',
-                '<img src="https://i.ibb.co/GVLFSxH/00.png" width="100%">',
-                '<img src="https://i.ibb.co/SmVF5vB/00.png" width="100%">',
-                '<img src="https://i.ibb.co/y6z683V/00.png" width="100%">',
-                '<img src="https://i.ibb.co/85r8z0f/9978a389246e31ed58bf4ebfc0cbe7ec.png" width="100%">',
-                '<img src="https://i.ibb.co/mC2pqc5/00.jpg" width="100%">',
-                '<img src="https://i.ibb.co/JQBmBXN/00.png" width="100%">',
-                '<img src="https://i.ibb.co/z5ZgQwN/00.png" width="100%">',
-                '<img src="https://i.ibb.co/dJZgXW2/image-snorlaxpng-pokemon-shuffle-wiki-fandom-powered-by-wikia-snorlax-png-256-256.png" width="100%">',
-                '<img src="https://i.ibb.co/LJTbjXd/00.gif" width="100%">',
-                '<img src="https://i.ibb.co/JHLQTz8/00.gif" width="100%">',
+                '<img src="https://i.imgur.com/EC1aFti.png" width="100%">',
+                '<img src="https://i.imgur.com/xwYW08t.png" width="100%">',
+                '<img src="https://i.imgur.com/l95Hipf.png" width="100%">',
+                '<img src="https://i.imgur.com/cFK5bpz.png" width="100%">',
+                '<img src="https://i.imgur.com/NhrbODk.png" width="100%">',
+                '<img src="https://i.imgur.com/yP5xC5s.png" width="100%">',
+                '<img src="https://i.imgur.com/xVwniUH.png" width="100%">',
+                '<img src="https://i.imgur.com/12UMOQ3.png" width="100%">',
+                '<img src="https://i.imgur.com/TcwJXqg.png" width="100%">',
+                '<img src="https://i.imgur.com/3SIpbR0.png" width="100%">',
             ]
         }
 
         function generarTablero() {
+            divMensaje.innerHTML = ``;
             cargarIconos()
             let len = iconos.length
             selecciones = []
@@ -49,18 +73,22 @@ let iconos = []
         }
 
         function seleccionarTarjeta(i) {
+    
             let tarjeta = document.getElementById("tarjeta" + i)
             if (tarjeta.style.transform != "rotateY(180deg)") {
                 tarjeta.style.transform = "rotateY(180deg)"
                 selecciones.push(i)
+                if(mensajes[i] != undefined){
+                    sendMensaje = mensajes[i]
+                }
             }
             if (selecciones.length == 2) {
-                deseleccionar(selecciones)
+                deseleccionar(selecciones, sendMensaje)
                 selecciones = []
             }
         }
 
-        function deseleccionar(selecciones) {
+        function deseleccionar(selecciones, mensaje='') {
             setTimeout(() => {
                 let trasera1 = document.getElementById("trasera" + selecciones[0])
                 let trasera2 = document.getElementById("trasera" + selecciones[1])
@@ -70,6 +98,7 @@ let iconos = []
                     tarjeta1.style.transform = "rotateY(0deg)"
                     tarjeta2.style.transform = "rotateY(0deg)"
                 }else{
+                    divMensaje.innerHTML = `<h1>${mensaje}</h1>`;
                     trasera1.style.background = "plum"
                     trasera2.style.background = "plum"
                 }
